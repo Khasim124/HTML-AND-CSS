16971302583d17823faf255024e20a2fd21956c5
YourGitHubUsername <Youremail@example.com>
2025-07-11 16:47:23 +0530

    HTML AND CSS

--- Java Script/JS/js day-1/clickbutton.js
+++
@@ -1,6 +0,0 @@
-function onClickEvent() {
-    const el = document.createElement('p');
-    el.innerText = 'Clicked the button';
-    document.querySelector('.container').appendChild(el);
-    }
-    document.querySelector('button').onclick = onClickEvent;
